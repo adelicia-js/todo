a937eb11181f25cd10e19fcf510ccd6fd45c1167
muneeb mubashir <muneeb.mubashir@emumba.com>
2024-03-15 01:30:52 +0500

    updated react app

--- src/App.js
+++ src/App.js
@@ -8,74 +8,66 @@ import React from 'react';
 
 import { MdAdd } from "react-icons/md";
 import { SlTrash } from "react-icons/sl";
-import { VscGithub } from "react-icons/vsc";
-import {SiLinkedin } from "react-icons/si";
+// import { VscGithub } from "react-icons/vsc";
+// import {SiLinkedin } from "react-icons/si";
 
-const TodoInput = ({ todo, setTodo, addTodo, setTodos, todos }) => {
-  return (
-    <div className="input-wrapper">
-      <input
-        type="text"
-        id="todo-input"
-        name="todo"
-        value={todo}
-        placeholder="Let's get workin'..."
-        onChange={(e) => {
-          setTodo(e.target.value);
-        }}
-        onKeyUp={(e) => {
-          if(e.key==='Enter') {
-            e.preventDefault();
-            setTodo(e.target.value);
-            setTodos([...todos, todo]);
-            setTodo('');
-          }
-        }}
-      ></input>
-      <button className="add-button" id="urmom" onClick={addTodo}>
-        <MdAdd size={21}/>
-      </button>
-    </div>
-  );
-};
+// const TodoInput = ({ todo, setTodo, addTodo, setTodos, todos }) => {
+//   return (
+//     <div className="input-wrapper">
+//       <input
+//         type="text"
+//         id="todo-input"
+//         name="todo"
+//         value={todo}
+//         placeholder="Let's get workin'..."
+//         onChange={(e) => {
+//           setTodo(e.target.value);
+//         }}
+//         onKeyUp={(e) => {
+//           if(e.key==='Enter') {
+//             e.preventDefault();
+//             setTodo(e.target.value);
+//             setTodos([...todos, todo]);
+//             setTodo('');
+//           }
+//         }}
+//       ></input>
+//       <button className="add-button" id="urmom" onClick={addTodo}>
+//         <MdAdd size={21}/>
+//       </button>
+//     </div>
+//   );
+// };
 
 
-const TodoList = ({ todoList, removeTodo }) => {
-  return (
-    <div className="input-list">
-      {todoList?.length > 0 ? (
-        <ul className="todo-list">
-          {todoList.map((entry, index) => (
-            <div className="todo">
-              <li key={index}>{entry}</li>
-              <button className="delete-button" onClick={()=>{removeTodo(entry)}}>
-                <SlTrash size={18}/>
+// const TodoList = ({ todoList, removeTodo }) => {
+//   return (
+//     <div className="input-list">
+//       {todoList?.length > 0 ? (
+//         <ul className="todo-list">
+//           {todoList.map((entry, index) => (
+//             <div className="todo">
+//               <li key={index}>{entry}</li>
+//               <button className="delete-button" onClick={()=>{removeTodo(entry)}}>
+//                 <SlTrash size={18}/>
 
-              </button>
-            </div>
-          ))}
-        </ul>
-      ) : (
-        <div className="empty">
-          <p>Add some tasks! :)</p>
-        </div>
-      )}
-    </div>
-  );
-};
+//               </button>
+//             </div>
+//           ))}
+//         </ul>
+//       ) : (
+//         <div className="empty">
+//           <p>Add some tasks! :)</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="love">Made with 💖</p>
-      <div className="socials">
-        <a href="https://github.com/adelicia-js" rel="noreferrer" target="_blank"><VscGithub size={25} /></a>
-        <a href="https://www.linkedin.com/in/adelicia/" rel="noreferrer" target="_blank"><SiLinkedin size={25} /></a>
-      </div>
       <ApiGatewayButton />
-      <p className="source">
-        <a href="https://github.com/adelicia-js/todo-cra" rel="noreferrer" target="_blank">$Source | 2023 - 2024</a>
-      </p>
     </footer>
   );
 };
@@ -86,7 +78,7 @@ const ApiGatewayButton = () => {
 
   return (
     <button className="api-gateway-button" onClick={() => window.open(apiGatewayUrl, '_blank')}>
-      Invoke API Gateway
+      Click to Invoke API Gateway
     </button>
   );
 };
@@ -124,10 +116,11 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Make a To-Do List!</h1>
+      <h1>Welcome to demo Emumba Data Intelligence Marketplace Product</h1>
+      <h2>This is the frontend of demo React App deployed using Amplify</h2>
       <div className="Content">
-        <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} setTodos={setTodos} todos={todos}/>
-        <TodoList todoList={todos} removeTodo={deleteTodo} />
+        {/* <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} setTodos={setTodos} todos={todos}/> */}
+        {/* <TodoList todoList={todos} removeTodo={deleteTodo} /> */}
       </div>
       <Footer/>
     </div>
